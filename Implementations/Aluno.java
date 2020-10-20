861f2a7fff8cb03b77ff7f1700bc810bb828674b
eikelennieandradedias <73168409+eikelennieandradedias@users.noreply.github.com>
2020-10-20 07:00:15 -0300

    Create Aluno.java
    Implementação classe aluno

---
+++ Implementations/Aluno.java
@@ -0,0 +1,36 @@
+
+
+public class Aluno {
+	private String nome;
+	private String email;
+    private double rendimento;
+
+
+public void imprimirRelatorio() {
+	
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public double getRendimento() {
+	return rendimento;
+}
+
+public void setRendimento(double rendimento) {
+	this.rendimento = rendimento;
+}
+}
